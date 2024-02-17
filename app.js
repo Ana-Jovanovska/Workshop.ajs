e0c0118083f30c1f401f02dcf768c405556506b4
Ana-Jovanovska <150249028+Ana-Jovanovska@users.noreply.github.com>
2024-02-17 10:57:37 +0100

    Workshop

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// People selector:
+// Person selector:
 const charactersContainerEl = document.querySelector(".characters-container");
 const yodaImageEl = document.querySelector(".yoda-image");
 const charactersButtonEl = document.createElement("button");
 
+//Ships selector:
+
+const shipsContainerEl = document.querySelector(".spaceship-container");
+const shipsImageEl = document.querySelector(".spaceship-image");
+const shipsButtonEl = document.createElement("button");
+
 // URL fetch (data):
-const PEOPLE_URL = "https://swapi.dev/api/people/?page=1";
+const PERSON_URL = "https://swapi.dev/api/people/?page=1";
 const SPACE_SHIPS_URL = "https://swapi.dev/api/starships/?page=1";
 
-//Fetch for people:
+//Fetch for person:
 
 const fetchPerson = async (url) => {
   try {
@@ -17,13 +23,11 @@ const fetchPerson = async (url) => {
     // console.log(data.results);
 
     renderPersonTable(data);
-    buttonsRenderPeople(yodaContainer, data, fetchStarWarsApi);
+    buttonsRenderPeople(charactersContainerEl, data, fetchPerson);
   } catch (error) {
     console.log(error);
   }
 };
-
-// This function will be crate a HTML table with persons data
 
 const generatePersonTable = (element, personDetalis) => {
   element.innerHTML = "";
@@ -57,23 +61,18 @@ const generatePersonTable = (element, personDetalis) => {
   `;
 };
 
-// Render function for persons table
-
 const renderPersonTable = (data) => {
-  generatePersonTable(yodaContainer, data.results);
+  generatePersonTable(charactersContainerEl, data.results);
 };
-// Add event listener for render table data for persons
 
-yodaImg.addEventListener("click", () => {
-  fetchPerson(PEOPLE_URL);
+yodaImageEl.addEventListener("click", () => {
+  fetchPerson(PERSON_URL);
 });
-
-// Function for crateing buttons for next and previous data and will work with callback function when is callled in fetch function.They will be create and will have function for displaying data in table when is clicked
 
 const buttonsRenderPeople = (element, data, callback) => {
   if (data.next) {
     const nextButtonPersons = document.createElement("button");
-    nextButtonPersons.innerText = "Next persons";
+    nextButtonPersons.innerText = "Next";
     nextButtonPersons.className = "nextBtnPersons";
     element.appendChild(nextButtonPersons);
     nextButtonPersons.addEventListener("click", () => {
@@ -82,11 +81,87 @@ const buttonsRenderPeople = (element, data, callback) => {
   }
   if (data.previous) {
     const previousButtonPersons = document.createElement("button");
-    previousButtonPersons.innerText = "Previous persons";
+    previousButtonPersons.innerText = "Previous";
     previousButtonPersons.className = "previousBtnPersons";
     element.appendChild(previousButtonPersons);
     previousButtonPersons.addEventListener("click", () => {
       callback(data.previous);
+    });
+  }
+};
+
+//Fetch for Ships:
+const fetchShips = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    // console.log(data.results);
+    renderShipTable(data);
+    buttonRenderShips(shipsContainerEl, data, fetchShips);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const generateShipsTable = (element, shipsDetalis) => {
+  element.innerHTML = "";
+  const rowHTML = (element.innerHTML = shipsDetalis.map((ship) => {
+    return `
+    <tr>
+    <td>${ship.name}</td>
+    <td>${ship.model}</td>
+    <td>${ship.manufacturer}</td>
+    <td>${ship.cost_in_credits}</td>
+    <td>${ship.passengers}</td>
+    <td>${ship.starship_class}</td>
+  </tr>`;
+  }));
+  element.innerHTML = `
+  <table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Model</th>
+      <th>Manufacturer</th>
+      <th>Cost (credits)</th>
+      <th>People Capacity (passengers)</th>
+      <th>Class</th>
+    </tr>
+  </thead>
+  <tbody>
+  ${rowHTML}
+  </tbody>
+</table
+`;
+};
+
+const renderShipTable = (data) => {
+  generateShipsTable(shipsContainerEl, data.results);
+};
+
+shipsImageEl.addEventListener("click", () => {
+  fetchShips(SPACE_SHIPS_URL);
+});
+
+const buttonRenderShips = (element, data, callback) => {
+  if (data.nextShips) {
+    const nextButtonShips = document.createElement("button");
+    nextButtonShips.innerHTML = "Next";
+    nextButtonShips.className = "nextBtnShips";
+    element.appendChild(nextButtonShips);
+    nextButtonShips.addEventListener("click", () => {
+      callback(data.next);
+    });
+  }
+
+  if (data.previous) {
+    const previousBtnShips = document.createElement("button");
+    previousBtnShips.innerHTML = "Previous";
+    previousBtnShips.className = "previousBtnShips";
+    element.appendChild(previousBtnShips);
+    previousBtnShips.addEventListener("click", () => {
+      callback(data.previousShips);
     });
   }
 };
